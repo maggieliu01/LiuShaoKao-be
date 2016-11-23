f871217b5cad72890f4016f194bd45fa5968fefd
Maggie Liu <maggie.liu01@sap.com>
2016-11-23 17:14:10 +0800

    keep update

--- com.lsk.admin.api/src/main/java/com/lsk/admin/api/TestAdminController.java
+++ com.lsk.admin.api/src/main/java/com/lsk/admin/api/TestAdminController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/admin")
+@RequestMapping(value = "/admin")
 public class TestAdminController {
 
-	@RequestMapping(value="/test")
+	@RequestMapping(value = "/test")
 	public String test() {
 		return "home";
 	}
