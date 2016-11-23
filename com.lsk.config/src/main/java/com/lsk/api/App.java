f05544ec8b806769fdbc92f9c9ddec55a401064a
Maggie Liu <maggie.liu01@sap.com>
2016-11-23 15:10:16 +0800

    nonsense changes

--- com.lsk.config/src/main/java/com/lsk/api/App.java
+++ com.lsk.config/src/main/java/com/lsk/api/App.java
@@ -1,14 +1,13 @@
 package com.lsk.api;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Configuration;abstract
 /**
  * @author I326998
  *
  */
 @Configuration
-@ComponentScan(basePackages="com.lsk.api")
+@ComponentScan(basePackages="com.lsk.admin.api")
 public class App 
 {
 	
--- com.lsk.config/src/main/java/com/lsk/api/interceptors/TestApiController.java
+++ com.lsk.config/src/main/java/com/lsk/api/interceptors/TestApiController.java
@@ -1,4 +1,4 @@
-package com.lsk.api;
+package com.lsk.api.interceptors;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
